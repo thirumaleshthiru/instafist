b9c630e28e97aa3cfc91f61bee494324ca6090ea
thirumaleshthiru <153974376+thirumaleshthiru@users.noreply.github.com>
2024-01-27 19:11:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,45 +2,40 @@ const express = require("express");
 const instagramDl = require("@sasmeee/igdl");
 const bodyParser = require("body-parser");
 const cors = require('cors');
+const Limiter = require('async-ratelimiter');
 
 const app = express();
-const PORT=  process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // You can change the port as needed
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/getInstagramData", async (req, res) => {
-  try {
-    const url = req.query.url;
+// Allow 1 request every 5 seconds
+const limiter = new Limiter({ max: 1, duration: 5000 });
 
-    if (!url) {
-      return res.status(400).json({ error: "URL parameter is required" });
+app.get("/getInstagramData", async (req, res) => {
+  const url = req.query.url;
+
+  if (!url) {
+    return res.status(400).json({ error: "URL parameter is required" });
+  }
+
+  try {
+    await limiter.removeTokens(1); // Wait until a token is available
+
+    const data = await instagramDl(url);
+
+    if (!data || data.length === 0) {
+      return res.status(404).json({ error: "No data found for the provided URL" });
     }
 
-    // Add a delay of 5 seconds before processing the URL
-    setTimeout(async () => {
-      try {
-        const data = await instagramDl(url);
-
-        if (!data || data.length === 0) {
-          return res.status(404).json({ error: "No data found for the provided URL" });
-        }
-
-        res.json(data);
-      } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "Something is Wrong" });
-      }
-    }, 3000); // 5000 milliseconds (5 seconds)
+    res.json(data);
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "Something is Wrong" });
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
- 
- 
-
