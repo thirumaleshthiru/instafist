bca990ba2cfe35d94bfce187be6bd90b0abb6714
thirumaleshthiru <153974376+thirumaleshthiru@users.noreply.github.com>
2024-01-27 19:23:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,65 +1,21 @@
-/*const express = require("express");
-const instagramDl = require("@sasmeee/igdl");
-const bodyParser = require("body-parser");
-const cors = require('cors');
-
-const app = express();
-const port = 3000; // You can change the port as needed
-
-app.use(bodyParser.json());
-app.use(cors());
-
-app.get("/getInstagramData", async (req, res) => {
-  try {
-    const url = req.query.url;
-
-    if (!url) {
-      return res.status(400).json({ error: "URL parameter is required" });
-    }
-
-    // Add a delay of 5 seconds before processing the URL
-    setTimeout(async () => {
-      try {
-        const data = await instagramDl(url);
-
-        if (!data || data.length === 0) {
-          return res.status(404).json({ error: "No data found for the provided URL" });
-        }
-
-        res.json(data);
-      } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "Something is Wrong" });
-      }
-    }, 3000); // 5000 milliseconds (5 seconds)
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Something is Wrong" });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
- */
-
-
 const express = require("express");
 const instagramDl = require("@sasmeee/igdl");
 const bodyParser = require("body-parser");
 const cors = require('cors');
-const Limiter = require('async-ratelimiter');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
-const port = process.env.PORT || 3000; // You can change the port as needed
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-// Allow 1 request every 5 seconds
-const limiter = new Limiter({ max: 1, duration: 5000 });
+const limiter = rateLimit({
+  windowMs: 5000, // 5 seconds
+  max: 1, // limit each IP to 1 request per 5 seconds
+});
 
-app.get("/getInstagramData", async (req, res) => {
+app.get("/getInstagramData", limiter, async (req, res) => {
   const url = req.query.url;
 
   if (!url) {
@@ -67,8 +23,6 @@ app.get("/getInstagramData", async (req, res) => {
   }
 
   try {
-    await limiter.removeTokens(1); // Wait until a token is available
-
     const data = await instagramDl(url);
 
     if (!data || data.length === 0) {
