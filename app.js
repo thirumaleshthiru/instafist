774185cd380fc3e46d1c7776c127bddb73db9be4
thirumaleshthiru <153974376+thirumaleshthiru@users.noreply.github.com>
2024-01-28 15:05:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,21 +2,35 @@ const express = require("express");
 const instagramDl = require("@sasmeee/igdl");
 const bodyParser = require("body-parser");
 const cors = require('cors');
+const Semaphore = require("semaphore-async");
 
 const app = express();
 const port = 3000;
+
+// Create a semaphore with a limit of 1 per IP address
+const semaphores = new Map();
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/getInstagramData", async (req, res) => {
-  const url = req.query.url;
+  const ip = req.ip;
+  const semaphore = semaphores.get(ip);
 
-  if (!url) {
-    return res.status(400).json({ error: "URL parameter is required" });
+  if (!semaphore) {
+    semaphore = new Semaphore(1);
+    semaphores.set(ip, semaphore);
   }
 
   try {
+    await semaphore.acquire();
+
+    const url = req.query.url;
+
+    if (!url) {
+      return res.status(400).json({ error: "URL parameter is required" });
+    }
+
     const data = await instagramDl(url);
 
     if (!data || data.length === 0) {
@@ -27,6 +41,8 @@ app.get("/getInstagramData", async (req, res) => {
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "Something is Wrong" });
+  } finally {
+    semaphore.release();
   }
 });
 
